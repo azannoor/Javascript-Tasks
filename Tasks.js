475ccc82c6aefafb9e124a3e091388dedde95fad
azannoor <azaanoor18@.com>
2024-03-19 01:20:34 -0700

    feat:"added object and array destruct func"

--- Tasks.js
+++ Tasks.js
@@ -19,3 +19,23 @@ const factorial = function(num1){
     }
     return temp
     }
+
+    console.log(factorial(6))
+
+// problem 3
+let people = [
+    { name: 'John', age: 30 },
+    { name: 'Alice', age: 25 },
+    { name: 'Bob', age: 35 }
+    ];
+
+   const [object1,object2,object3] = people
+console.log(object1,object2,object3)
+
+const {name:name1,age:Age1} = object1
+const {name:name2,age:Age2} = object2
+const {name:name3,age:Age3} = object3
+
+console.log(name1,Age1)
+console.log(name2,Age2)
+console.log(name3,Age3)
